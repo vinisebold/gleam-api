f632722f4b96d1387830e7ffa9e831b15724e16f
ISAAC ELIAS SILVA DOS SANTOS <isaac_e_santos@estudante.sesisenai.org.br>
2025-08-08 14:51:15 -0300

    firstCommit

---
+++ src/test/java/com/gleam/Gleam/GleamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleam.Gleam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GleamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
