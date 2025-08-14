0002250f0a27e1b149738ed41573894f932f91ea
vinisebold <vinicius.sebold05@gmail.com>
2025-08-14 15:58:44 -0300

    rollback: reverte alterações do commit anterior

--- src/main/java/com/gleamTeste/Gleamteste/repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gleamTeste.Gleamteste.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.gleamTeste.Gleamteste.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
-
