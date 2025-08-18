18fbfbf62f17e59f19158967b6562f734d5ef17e
VINÍCIUS GOMES SENS <vinicius_sens@estudante.sesisenai.org.br>
2025-08-18 14:57:03 -0300

    fix(app): Update imports in BackendApplication

--- src/main/java/com/gleam/backend/BackendApplication.java
+++ src/main/java/com/gleam/backend/BackendApplication.java
@@ -1,8 +1,19 @@
 package com.gleam.backend;
 
+import com.gleam.backend.enums.Acabamento;
+
+import com.gleam.backend.model.Cliente;
+import com.gleam.backend.model.Fornecedor;
+import com.gleam.backend.model.Produto;
+
+import com.gleam.backend.repository.ClienteRepository;
+import com.gleam.backend.repository.FornecedorRepository;
+import com.gleam.backend.repository.ProdutoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @EnableCaching
 @SpringBootApplication
