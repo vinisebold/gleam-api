891cd317d244b2a21cd3d0dc9da15c3a8bd43c7b
Vinicius Sebold <63889194+vinisebold@users.noreply.github.com>
2025-08-19 13:49:46 -0300

    fix(WebConfig): problema de sintaxe

--- src/main/java/com/gleam/backend/config/WebConfig.java
+++ src/main/java/com/gleam/backend/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:4200",
                         "https://didactic-space-capybara-rwgp7p7rvvgfpg79-4200.app.github.dev",
                         "https://entra21-frontend-tcc.vercel.app",
-                        "https://4200-firebase-entra21-frontend-tcc-1755514396990.cluster-l2bgochoazbomqgfmlhuvdvgiy.cloudworkstations.dev/",
+                        "https://4200-firebase-entra21-frontend-tcc-1755514396990.cluster-l2bgochoazbomqgfmlhuvdvgiy.cloudworkstations.dev"
                 )
                 // Define os métodos HTTP permitidos (GET, POST, PUT, DELETE, etc.)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
