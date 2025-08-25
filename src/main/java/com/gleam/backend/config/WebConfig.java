873d555d113fdc16cb30ebea945db5b658122879
Vinicius Sebold <63889194+vinisebold@users.noreply.github.com>
2025-08-25 08:44:52 -0300

    Update allowed origins in WebConfig

--- src/main/java/com/gleam/backend/config/WebConfig.java
+++ src/main/java/com/gleam/backend/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
                 // Define as origens permitidas para fazer requisições
                 .allowedOrigins(
                         "http://localhost:4200",
-                        "https://didactic-space-capybara-rwgp7p7rvvgfpg79-4200.app.github.dev",
+                        "https://ominous-goldfish-9w9j6j6qv6pf4rp-4200.app.github.dev",
                         "https://entra21-frontend-tcc.vercel.app",
                         "https://4200-firebase-entra21-frontend-tcc-1755514396990.cluster-l2bgochoazbomqgfmlhuvdvgiy.cloudworkstations.dev"
                 )
